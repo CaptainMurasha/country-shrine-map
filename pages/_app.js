49307bb2f148869d0974023597c481f653c886b2
CaptainMurasha <murasha.minamitsu@gmail.com>
2021-04-29 21:18:04 +0900

    bootstrap and js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/global.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.css' // css読み込みは後勝ち
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,49 @@
-// ここに一覧的なオブジェクトを作ってforで回す
-
 import Head from 'next/head';
 import Link from 'next/link';
 
-
+const categories = [
+  {link:"ishidan", name:"石段"},
+  {link:"komainu", name:"狛犬"},
+  {link:"torii", name:"鳥居"},
+  {link:"sugibayashi", name:"杉林"},
+  {link:"hikyou", name:"秘境"},
+  {link:"umi", name:"海"},
+  {link:"yama", name:"山"},
+  {link:"tanbo", name:"田んぼ"},
+  {link:"koke", name:"苔"},
+  {link:"takebayashi", name:"竹林"},
+  {link:"iwakabe", name:"岩壁"},
+  {link:"kyoboku", name:"巨木"}
+]
+  
 export default function home() {
+
+  // 画像リスト生成
+  const categoryList = [];
+  categories.forEach(category => {
+    categoryList.push(
+      <div className="col-4" key={category.link}>
+        <Link 
+          href={`/articles/${category.link}`}><a><img src={`/images/top/${category.name}.jpeg`}
+          alt={category.name}
+          title={category.name} /></a></Link></div>
+    );
+  });
+
   return (
     <>
       <Head>
-        <title>田舎神社へ行こう</title>
+        <title >田舎神社へ行こう</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=yes,maximum-scale=2.0" />
-      <h1>田舎神社へ行こう</h1>
-      <ul className="shrine-tiles">
-        <li><Link href="/articles/ishidan"><a><img src="/images/top/石段.jpeg" alt="石段" title="石段" /></a></Link></li>
-        <li><Link href="/articles/torii"><a><img src="/images/top/鳥居.jpeg" alt="鳥居" title="鳥居" /></a></Link></li>
-        <li><Link href="/articles/komainu"><a><img src="/images/top/狛犬.jpeg" alt="狛犬" title="狛犬" /></a></Link></li>
-      </ul>
-      <ul className="shrine-tiles">
-        <li><Link href="/articles/sugibayashi"><a><img src="/images/top/杉林.jpeg" alt="杉林" title="杉林" /></a></Link></li>
-        <li><Link href="/articles/hikyou"><a><img src="/images/top/秘境.jpeg" alt="秘境" title="秘境" /></a></Link></li>
-        <li><Link href="/articles/umi"><a><img src="/images/top/海.jpeg" alt="海" title="海" /></a></Link></li>
-      </ul>
-      <ul className="shrine-tiles">
-        <li><Link href="/articles/yama"><a><img src="/images/top/山.jpeg" alt="山" title="山" /></a></Link></li>
-        <li><Link href="/articles/tanbo"><a><img src="/images/top/田んぼ.jpeg" alt="田んぼ" title="田んぼ" /></a></Link></li>
-        <li><Link href="/articles/koke"><a><img src="/images/top/苔.jpeg" alt="苔" title="苔" /></a></Link></li>
-      </ul>
-      <ul className="shrine-tiles">
-        <li><Link href="/articles/takebayashi"><a><img src="/images/top/竹林.jpeg" alt="竹林" title="竹林" /></a></Link></li>
-        <li><Link href="/articles/iwakabe"><a><img src="/images/top/岩壁.jpeg" alt="岩壁" title="岩壁" /></a></Link></li>
-        <li><Link href="/articles/kyoboku"><a><img src="/images/top/巨木.jpeg" alt="巨木" title="巨木" /></a></Link></li>
-      </ul>
+      <div className="page-header">
+        <h1>田舎神社へ行こう  <small>Let's go to rural shrine.</small></h1>
+      </div>
+      <div className="row shrine-tiles">
+        {categoryList}
+      </div>
     </>
   );
 };
